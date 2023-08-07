c25269abc34f1cfcfb67ad57013651690b8b7895
ajayythakur <ajay1270panwar@gmail.com>
2023-08-07 17:18:32 +0530

    code updated

--- index.js
+++ index.js
@@ -1,190 +1,165 @@
-const arrayObj=[];
-const miniObjArray = [];
+var arr_of_obj = new Set();
+var value_id;
+var title_flag = false;
+var subtask = new Map;
 
-let btn1=document.getElementById("btn1");
-let btn2=document.getElementById("btn2");
-// let input1;
+//popup to add card
+function modal(){
+    document.getElementById("modal-div").style.display = "block";
+    document.getElementById("header").style.filter="blur(5px)";
+};
 
-// btn1.addEventListener("click",function(){
-//     let inputbox=document.getElementById("inputbox").value;
-//     input1=inputbox;
-//     console.log(input1);
-//     let div=document.createElement("div")
-//     div.setAttribute("id","card")
-//     console.log("hye")
+//add button in popup of add card
+function addCard(){
+    var card_title = document.getElementById("modal-input-box").value;
+    createObj(card_title);
+    closeModal();
+}
 
-//     let parent=document.getElementById("parent");
-//     // parent.appendChild(div);
-//     parent.innerText=input1;
-// })
-function addTaskInput() {
-    document.getElementById("container").style.filter = "blur(5px)";
-    document.getElementById("focus").style.visibility ="visible";
-      document.getElementById("homepgtxt").style.display = "none";
-  }
+//close button in popup of add card
+function closeModal(){
+    document.getElementById("modal-div").style.display = "none";
+    document.getElementById("header").style.filter="blur(0px)"
+}
 
-function addDataObj(){
-    let invalue = document.getElementById("inputbox").value;
-  
-    let objData = {
-      id: Date.now(),
-      taskName: invalue,
+//assign card detais to set
+function createObj(title){
+    document.getElementById('empty-list').style.display = 'none'
+    var card_obj = {
+        title: title,
+        id: Date.now(),
+        subtask
+    };
+    arr_of_obj.add(card_obj);
+    createCard(card_obj.id);
+};
+
+//cloning of subtask
+function addList(){
+    var cloned_list_item = document.querySelector(".this-list-element").cloneNode(true);
+    var card_item = document.getElementById('modal-input-box-card').value;
+    console.log(value_id);
+    cloned_list_item.innerText =  card_item; 
+    cloned_list_item.style.display = "block";
+    cloned_list_item.setAttribute('id',`${Date.now()}`);
+    cloned_list_item.setAttribute('value',`${Date.now()}`);
+    cloned_list_item.setAttribute('style',"margin-left: 10px;");
+    var done_button = document.createElement('button');
+    done_button.setAttribute('id',`check-done-${Date.now()}`);
+    done_button.setAttribute('class','mark-as-done-class');
+    done_button.setAttribute('value',`${Date.now()}`);
+    done_button.setAttribute('onclick','completedTask(this.value)');
+    done_button.innerText = ' Mark Done';
+    done_button.setAttribute('style','font-size:15 px;cursor:pointer; height:18px; border-radius:10px;')
+    cloned_list_item.appendChild(done_button);
+    cloned_list_item.setAttribute('onClick',"completedTask(this.value)");
+     for(obj of arr_of_obj){
+        for(prop in obj){
+            if(obj.id == value_id){
+                obj.subtask.set(`${card_item}`,`${Date.now()}`);
+                break;
+            }
+        }
     }
-  
-    arrayObj.push(objData);
-    addCard()
-  
-  }
+    document.getElementById(`${value_id}`).getElementsByClassName('add-list-after-this')[0].appendChild(cloned_list_item).appendChild(done_button);
+    closeCardModal();
+}
+//close item box 
+function closeCardModal(){
+    document.getElementById('modal-div-card').style.display = "none";
+}
+//adding sublist items to card
+function addSubtask(val) {
+    document.getElementById("modal-div-card").style.display = "block";
+    value_id = val;
+};
 
-  function addCard() {
-   
-    let divElement = document.createElement("div")
-    let headDiv = document.createElement("div");
-    let middleDiv = document.createElement("div");
-    let iconDiv = document.createElement("div");
-    let h = document.createElement("h1");
-    let iconAdd = document.createElement("i");
-    let iconDelete = document.createElement("i");
-
-    divElement.setAttribute("class", "card-1");
-    divElement.setAttribute("id", `cardId${Date.now()}`);
-    headDiv.setAttribute("class", "head-div");
-    headDiv.setAttribute("id", "head-div-Id");
-    middleDiv.setAttribute("class", "middle-div");
-    middleDiv.setAttribute("id", "middle-div-Id");
-    iconDiv.setAttribute("class", "icon-div");
-    iconDiv.setAttribute("id", "icon-div-Id");
-    iconAdd.setAttribute("class", "fas fa-plus-circle");
-    iconAdd.setAttribute("id", "add-icon");
-    iconDelete.setAttribute("class", "fas fa-trash-alt");
-    iconDelete.setAttribute("id", "deleteIcon");
+//delete particular card
+function deleteCard(val){
+    var delete_div = document.getElementById(`${val}`);
+    for(obj of arr_of_obj){
+        for(prop in obj){
+        if (obj.id==val)
+        arr_of_obj.delete(obj);
+        break;
+        }
+    }
+    delete_div.parentNode.removeChild(delete_div);
+    if(arr_of_obj.size==0){
+        document.getElementById('empty-list').style.display = 'block';
+    }
     
-    let headingText = document.createTextNode(arrayObj[arrayObj.length-1].taskName);
-     h.appendChild(headingText)
-     headDiv.appendChild(h);
-     divElement.appendChild(headDiv);
-     divElement.appendChild(middleDiv);
-     iconDiv.appendChild(iconDelete);
-     iconDiv.appendChild(iconAdd);
-     divElement.appendChild(iconDiv);
+};
+//cloning of card
+function createCard(){
+    var first_card = document.querySelector('.card').cloneNode(true);
+    display(first_card);
+};
 
-
-
-
-     //deleting the card
-
-  iconDelete.addEventListener('click', ()=>{
-    divElement.remove();
-  })
-
-  //creating again mini item
-  iconAdd.addEventListener('click', ()=>{
-    document.getElementById("container").style.filter = "blur(8px)";
-  document.getElementById("focus2").style.visibility =
-    "visible";
-
-  })
-
-  let btn1_i = document.getElementById("btn1-i");
-  btn1_i.addEventListener("click", miniItemObj);
-
-
-
-     
-    document.getElementById("list-container").appendChild(divElement); 
-    document.getElementById("focus").style.visibility =  "hidden";
-    
-  document.getElementById("container").style.filter = "blur(0px)";
-
-  
-  headDiv.addEventListener('click', ()=>{
-    document.getElementById("heading1").style.visibility="hidden";
-    document.getElementById("middletxt").style.fontSize="70px";
-    document.getElementById("middletxt").innerText=h.innerText;
-    document.getElementById("backbtn").style.visibility="visible";
-    backbtn.addEventListener("click",()=>{
-      // addCard();
-      document.getElementById("heading1").style.visibility="visible";
-      document.getElementById("middletxt").style.visibility="hidden";
-      document.getElementById("backbtn").style.visibility="hidden";
-
+//task list item done
+function completedTask(value){
+    document.getElementById(`${value}`).style.textDecoration = 'line-through';
+    document.getElementById(`${value}`).style.color = '#112D4E';
+    document.getElementById(`check-done-${value}`).remove();
+}
+//appending cards to outer container
+function display(card){
+    document.getElementById('empty-list').style.display = 'none'
+    arr_of_obj.forEach(element => {
+        card.id = element.id;
+        card.querySelector(".card-head").innerHTML = element.title;
+        card.querySelector(".card-head").setAttribute('value',`${element.id}`);
+        card.setAttribute("value",`${element.id}`);
+        card.setAttribute("display","block");
+        card.setAttribute("min-height","300px");
+        card.querySelector(".delete-button-in-card").setAttribute("value",`${element.id}`);
+        card.querySelector(".delete-button-in-card").setAttribute("onClick","deleteCard(this.value)");
+        card.querySelector(".add-button-in-card").setAttribute("value",`${element.id}`);
+        card.querySelector(".add-button-in-card").setAttribute("onClick","addSubtask(this.value)");    
     });
-    // document.getElementById("heading1").style.display = "none";
-    document.getElementById("card-header-content").style.display = "block";
-    document.getElementById("middle-mini-item-heading").innerHTML = arrayObj[arrayObj.length-1].taskName;
-    document.getElementById("addcard").innerText = "";
-    document.getElementById("list-container").style.visibility =  "hidden";
-    divElement.style.visibility = "visible";
-    document.getElementById("middle-mini-item-heading").style.fontSize = "50px";
-  })
-  }
-
-// 2nd half
-function closeDataObj()
-{
-  document.getElementById("container").style.filter = "blur(0px)";
-  document.getElementById("focus").style.visibility ="hidden";
-    document.getElementById("homepgtxt").style.display = "none";
+    if(title_flag)
+    card.style.display = 'none';
+    else
+    card.style.display = "block";
+    document.getElementById("outer-container").appendChild(card);
 }
+//selected card on entire page
+function headerFunc(val){
+    var card_header;
+    for(let ele of arr_of_obj){
+        for(let id in ele){
+            if(ele[id]==val){
+                card_header = ele.title;
+                break;
+            };
+        };
+    };
+    document.querySelector("#app-name").style.display = 'none';
+    document.querySelector("#add-button-text").style.display = 'none';
+    for(let ele of arr_of_obj){
+            if(ele.id==val){
+                document.getElementById(`${ele.id}`).style.display = 'block';
+            }
+            else {
+                document.getElementById(`${ele.id}`).style.display = 'none';
+            }
+    };
+    document.getElementById('card-dynamic-head').innerText = `${card_header}`;
+    document.getElementById('card-dynamic-head').style.display = 'flex'
+    document.getElementById('outer-container').style.justifyContent = 'center'
+    document.getElementById('back-button').style.display = 'block'
+    title_flag = true;
+};
 
-let btn2_i=document.getElementById("btn2-i").addEventListener("click",()=>{
-  document.getElementById("container").style.filter = "blur(0px)";
-  document.getElementById("focus").style.visibility ="hidden";
-    document.getElementById("homepgtxt").style.display = "none";
-    document.getElementById("focus2").style.visibility ="hidden";
-
-
-})
-
-
-function miniItemObj(){
-  let inputValue = document.getElementById("inputbox2").value;
-  let miniObj = {
-    id: Date.now(),
-    miniItemName: inputValue,
-  }
-
-  miniObjArray.push(miniObj);
-  addItem()
-
-
-}
-
-function addItem(){
-  
-  
-  let miniItemDiv = document.createElement("div");
-  let itemSpan = document.createElement("span");
-  let cancelItemSpan = document.createElement("span");
-  let v = document.createTextNode(miniObjArray[miniObjArray.length-1].miniItemName);
-  let i = document.createTextNode("Mark Done");
-
-
-  miniItemDiv.setAttribute("class", "mini-item-div");
-  miniItemDiv.setAttribute("id", `${Date.now()}`)
-  itemSpan.setAttribute("id", `mini-item${Date.now()}`);
-  cancelItemSpan.setAttribute("id", `cancel-item${Date.now()}`);
-
-
-  miniItemDiv.appendChild(itemSpan);
-  miniItemDiv.appendChild(cancelItemSpan);
-  itemSpan.appendChild(v);
-  cancelItemSpan.appendChild(i);
-  document.getElementById("middle-div-Id").appendChild(miniItemDiv);
- 
-
-
-
-
-  cancelItemSpan.addEventListener('click', ()=>{
-    cancelItemSpan.remove();
-    itemSpan .style.textDecoration = "line-through";
-    itemSpan .style.color = "red";
-
-  })
-
-  document.getElementById("focus2").style.visibility =  "hidden";
-    
-  document.getElementById("container").style.filter = "blur(0px)";
-
+//after clicking back button main page
+function displayAll(){
+    title_flag = false;
+    document.querySelector("#app-name").style.display = 'block';
+    document.querySelector("#add-button-text").style.display = 'inline-block';
+    document.getElementById('back-button').style.display = 'none';
+    for(let ele of arr_of_obj){
+            document.getElementById(`${ele.id}`).style.display = 'block';
+    };
+    document.getElementById('card-dynamic-head').innerText = ``;
+    document.getElementById('card-dynamic-head').style.display = 'none';
 }
